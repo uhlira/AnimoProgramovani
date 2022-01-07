f86a2b38aca4fd5a1d5bd26db806b77d28896cf9
Adam Uhlíř <uhlira@seznam.cz>
2022-01-07 20:00:37 +0100

    added diary

---
+++ Diary/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diary diary = new Diary();
+            DiaryAction action = DiaryAction.EXIT_APP;
+
+            do
+            {
+                PrintMenu();
+
+                action = ReadMenuKey();
+
+                switch (action)
+                {
+                    case DiaryAction.ADD_RECORD: diary.Add(PrintAddRecord());
+                        break;
+                    case DiaryAction.PRINT_RECORDS:
+                        {
+                            foreach (Record r in diary.Records)
+                            {
+                                Console.WriteLine(r.ToString());
+                            }
+                        }
+                        break;
+                    case DiaryAction.SAVE_DIARY:
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            } while (action != DiaryAction.EXIT_APP);
+
+            //Console.ReadKey();
+        }
+
+        public static void PrintMenu() 
+        {
+            Console.WriteLine("DIARY MENU");
+            Console.WriteLine("1 - ADD RECORD");
+            Console.WriteLine("2 - PRINT RECORDS");
+            Console.WriteLine("3 - SAVE DIARY");
+
+            Console.WriteLine("0 - EXIT APP");
+        }
+
+        public static DiaryAction ReadMenuKey()
+        {
+            switch (Console.ReadKey().KeyChar) 
+            {
+                case '1': return DiaryAction.ADD_RECORD;
+                case '2': return DiaryAction.PRINT_RECORDS;
+                case '3': return DiaryAction.SAVE_DIARY;
+
+                case '0': return DiaryAction.EXIT_APP;
+
+                default: return DiaryAction.PRINT_RECORDS;
+            }
+        }
+
+        public enum DiaryAction 
+        {
+            ADD_RECORD, 
+            PRINT_RECORDS, 
+            SAVE_DIARY, 
+            EXIT_APP
+        }
+
+        public static Record PrintAddRecord()
+        {
+            Record r = new Record() { RecordTime = DateTime.Now };
+
+            Console.Write("Autor: ");
+            r.Autor = Console.ReadLine();
+
+            Console.Write("Title: ");
+            r.Title = Console.ReadLine();
+
+            Console.Write("Text: ");
+            r.Text = Console.ReadLine();
+
+            return r;
+        }
+    }
+}
---
+++ Diary/Record.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diary
+{
+    public class Record
+    {
+
+        DateTime recordTime;
+        public DateTime RecordTime // { get; set; }
+        {
+            get 
+            { 
+                return recordTime; 
+            }
+
+            set 
+            { 
+                if(DateTime.Now - value < new TimeSpan(0, 5, 0))
+                    recordTime = value; 
+            }
+        }
+
+        string title;
+        public string Title
+        {
+            get { return title; }
+
+            set
+            {
+                if (value.Length < 50)
+                    title = value;
+            }
+        }
+
+        string text;
+        public string Text 
+        {
+            get { return text; }
+
+            set
+            {
+                if (value.Length < 50000)
+                    text = value;
+            }
+        }
+
+        string autor;
+        public string Autor
+        {
+            get { return autor; }
+
+            set
+            {
+                if (value.Length < 200)
+                    autor = value;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            Record r = obj as Record;
+
+            if (r != null) 
+            {
+                return Title == r.Title && Text == r.Text && Autor == r.Autor && RecordTime == r.RecordTime;
+            }
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return RecordTime.ToString("dddd, dd MMMM yyyy HH:mm:ss") + "\t" +
+                Autor + "\t" +
+                Title + "\t" +
+                Text;
+        }
+    }
+
+    public class Diary
+    {
+        private List<Record> records;
+        public List<Record> Records 
+        {
+            get 
+            {
+                return new List<Record>(records);
+            }
+            private set 
+            {
+                records = value;
+            } 
+        }
+
+        public Diary()
+        {
+            this.Records = new List<Record>();
+        }
+
+        public Diary(List<Record> records)
+        {
+            this.Records = records;
+        }
+
+        public void Add(Record record)
+        {
+            Record rempty = new Record()
+            {
+                RecordTime = DateTime.Now,
+                Title = string.Empty,
+                Text = string.Empty,
+                Autor = string.Empty
+            };
+
+            if (!record.Equals(rempty))
+                records.Add(record);
+        }
+    }
+}
